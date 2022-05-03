b35a4358b13bfbfed631625a7e91ee122efa16e8
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-05-03 15:44:58 +0530

    Add support to roll spare

---
+++ src/Bowling.Core/BowlingGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Bowling.Core
+{
+    public class BowlingGame
+    {
+        private List<int> rolls = new List<int>();
+
+        public void Roll(int pins)
+        {
+            rolls.Add(pins);
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int rollIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                score += rolls[rollIndex] + rolls[rollIndex + 1];
+
+                rollIndex += 2;
+            }
+
+            return score;
+        }
+    }
+}
---
+++ test/Bowling.UnitTests/Core/BowlingGameTests.cs
@@ -0,0 +1,37 @@
+﻿using Bowling.Core;
+using Xunit;
+
+namespace Bowling.UnitTests.Core
+{
+    public class BowlingGameTests
+    {
+        [Fact]
+        public void Score_should_be_0_when_rolled_all_0()
+        {
+            var game = new BowlingGame();
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.Equal(0, game.GetScore());
+        }
+
+        [Fact]
+        public void Score_should_be_10_when_rolled_one_spare()
+        {
+            var game = new BowlingGame();
+
+            game.Roll(3);
+            game.Roll(7);
+
+            for (int i = 2; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.Equal(10, game.GetScore());
+        }
+    }
+}
